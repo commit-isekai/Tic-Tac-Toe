8cb6c735ee57cfe9783f292c64ae5e405605327e
kui <thebazz.98@gmail.com>
2020-02-02 15:49:45 +0200

    added togglable player selection

--- src/index.js
+++ src/index.js
@@ -1,2 +1,6 @@
 /* eslint linebreak-style: ["error", "windows"] */
 import './scss/main.scss'
+
+document.querySelector('.choose-player-buttons').addEventListener('click', (e) => {
+    console.log(e.target.classList.toggle('active'))
+})
