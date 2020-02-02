98bd1b3c641bcad12099474d509021dd8eb951f8
kui <thebazz.98@gmail.com>
2020-02-02 17:30:26 +0200

    Initial dist subtree commit

--- src/index.js
+++
@@ -1,6 +0,0 @@
-/* eslint linebreak-style: ["error", "windows"] */
-import './scss/main.scss'
-
-document.querySelector('.choose-player-buttons').addEventListener('click', (e) => {
-    console.log(e.target.classList.toggle('active'))
-})
