1f97c84f3dc49d9425ed29a9bb254b542705e0cf
kui <thebazz.98@gmail.com>
2020-02-05 12:07:26 +0200

    added togglable player mode

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@
 import './scss/main.scss'
 
 document.querySelector('.choose-player-buttons').addEventListener('click', (e) => {
-    console.log(e.target.classList.toggle('active'))
+  console.log(e.target.classList.toggle('active'))
+  
+  document.querySelector(".difficulty-and-second-player").classList.toggle('active')
 })
 
 document
